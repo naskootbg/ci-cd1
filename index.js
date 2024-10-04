20c61a91feb5b63fa706c454173960a19c7eb4dd
naskootbg <support.bnt@gmail.com>
2024-10-04 12:50:21 +0300

    test2

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.listen(port, () => {
 })
 .on('error', function(err) {
   if (err.errno === 'EADDRINUSE')
-    console.error(`Port ${port} too busy.`);
+    console.error(`Port ${port} too busy now.`);
   else 
     throw err;
 });
